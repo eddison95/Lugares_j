e5ee224d897633a99ae8e61616ef197c2cf2dd8c
eddison <eddisonmo95@gmail.com>
2022-10-11 22:48:04 -0600

    Tercera version semana 3. Proyecto nuevo

--- app/src/main/java/com/lugares_j/ui/lugar/AddLugarFragment.kt
+++ app/src/main/java/com/lugares_j/ui/lugar/AddLugarFragment.kt
@@ -1,22 +1,21 @@
 package com.lugares_j.ui.lugar
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.fragment.findNavController
-import com.lugares_j.R
+import com.lugares_j.databinding.FragmentAddLugarBinding
 import com.lugares_j.databinding.FragmentLugarBinding
 import com.lugares_j.viewmodel.LugarViewModel
 
-class LugarFragment : Fragment() {
+class AddLugarFragment : Fragment() {
 
-    private var _binding: FragmentLugarBinding? = null
+    private var _binding: FragmentAddLugarBinding? = null
 
-    // This property is only valid between onCreateView and
+    // This pr
+    // operty is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
 
@@ -28,11 +27,9 @@ class LugarFragment : Fragment() {
         val homeViewModel =
             ViewModelProvider(this).get(LugarViewModel::class.java)
 
-        _binding = FragmentLugarBinding.inflate(inflater, container, false)
-     //   val root: View = binding.root
-        binding.addLugarFabButton.setOnClickListener {
-               findNavController().navigate(R.id.action_nav_lugar_to_addLugarFragment) // Boton de agregar redirije a pantalla para insertar
-        }
+        _binding = FragmentAddLugarBinding.inflate(inflater, container, false)
+        //   val root: View = binding.root
+
 
         return binding.root
     }
